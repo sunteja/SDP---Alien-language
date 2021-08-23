c7d17bc1d8f8e072974cacbedbae38ed2e958e77
@sunteja <suryaiiitb@gmail.com>
2021-08-23 14:46:47 +0530

    Wrote helper functions for the succ_alien functions

--- alienv3.cpp
+++ alienv3.cpp
@@ -1,42 +1,57 @@
-// In this code there are better variable names and the variables which are getting repeated often like b.length() and n.length() are taken into other variable names like bl, nl etc. This makes the code more understandable and clean. Moreover, the variable names like symtonstr, nchartosym, answer are meaningful as the former is a map which stores the mapping from symbols to string of numbers and the latter stores the mapping from integer characters like '1', '2' to the alien symbols.
+// In this code all the functions are written independently of each other. The numtoal function converts a string from the integer string to the corresponding alien string. On the other hand, the altonum does the reverse. The succutil function is a utility function which adds 1 to the string given to it and stores it in the original string itself.
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
+void numtoal(string &str, unordered_map<char, char> n) {
+	ll nl = str.length();
+	for (ll i = 0; i < nl; i++) {
+		str[i] = n[str[i]];
+	}
+}
+
+string altonum(string str, unordered_map<char, string> m) {
+	ll nl = str.length();
+	string answer = "";
+	for (ll i = 0; i < nl; i++) {
+		answer = answer + m[str[i]];
+	}
+	return answer;
+}
+
+void succutil(string &str, ll bl) {
+	ll nl = str.length();
+	ll i = nl - 1;
+	ll carry = 0;
+	while (i >= 0) {
+		char old = str[i];
+		if (i == nl - 1)
+			str[i] = (carry + (old - '0') + 1) % (bl) + '0';
+		else {
+			str[i] = (carry + (old - '0')) % (bl) + '0';
+		}
+		carry = ((old - '0') + 1) / (bl);
+		i--;
+	}
+	if (carry >= 1) {
+		cout << "Overflow detected..." << endl;
+		str = to_string(carry) + str;
+	}
+}
 string succ_alien(string n, string b) {
 	unordered_map<char, string> symtonstr;
 	unordered_map<char, char> nchartosym;
 	ll bl = b.length();
 	ll nl = n.length();
-	string answer = "";
 	for (ll i = 0; i < bl; i++) {
 		symtonstr[b[i]] = to_string(i);
 	}
 	for (ll i = 0; i < bl; i++) {
 		nchartosym[i + '0'] = b[i];
 	}
-	for (ll i = 0; i < nl; i++) {
-		answer = answer + symtonstr[n[i]];
-	}
-	ll i = nl - 1;
-	ll carry = 0;
-	while (i >= 0) {
-		char old = answer[i];
-		if (i == nl - 1)
-			answer[i] = (carry + (old - '0') + 1) % (bl) + '0';
-		else {
-			answer[i] = (carry + (old - '0')) % (bl) + '0';
-		}
-		carry = ((old - '0') + 1) / (bl);
-		i--;
-	}
-	for (ll i = 0; i < nl; i++) {
-		answer[i] = nchartosym[answer[i]];
-	}
-	if (carry >= 1) {
-		cout << "Overflow detected..." << endl;
-		answer = nchartosym[carry + '0'] + answer;
-	}
+	string answer = altonum(n, symtonstr);
+	succutil(answer, bl);
+	numtoal(answer, nchartosym);
 	return answer;
 }
 
